085af1b910bb09402eda5ec850fa611d18e2ce3a
TVernet <112699327+TVernet@users.noreply.github.com>
2024-06-30 16:13:10 +0200

    Update path_nfc_file.py

--- path_nfc_file.py
+++ path_nfc_file.py
@@ -3,21 +3,21 @@
 # after each new use of the NFC badge.** 
 
 # Path to Maison_1.nfc 
-date_file_1 = "27/06/2024"
-home_1_file_path = "C:\\Users\\tiffv\\Desktop\\CYBER\\Flipper_zero\\NFC_Python\\nfc_files\\Maison_1.nfc"
+date_file_1 = ""
+home_1_file_path = ""
 
 # Path to Maison_2.nfc 
-date_file_2 = "27/06/2024"
-home_2_file_path = "C:\\Users\\tiffv\\Desktop\\CYBER\\Flipper_zero\\NFC_Python\\nfc_files\\Maison_2.nfc"
+date_file_2 = ""
+home_2_file_path = ""
 
 # Path to Maison_3.nfc - 
-date_file_3 = "28/06/2024"
-home_3_file_path = "C:\\Users\\tiffv\\Desktop\\CYBER\\Flipper_zero\\NFC_Python\\nfc_files\\Maison_3.nfc"
+date_file_3 = ""
+home_3_file_path = ""
 
 # Path to Maison_4.nfc  
-date_file_4 = "28/06/2024"
-home_4_file_path = "C:\\Users\\tiffv\\Desktop\\CYBER\\Flipper_zero\\NFC_Python\\nfc_files\\Maison_4.nfc"
+date_file_4 = ""
+home_4_file_path = ""
 
 # Path to Maison_5.nfc  
-date_file_5 = "29/06/2024"
-home_5_file_path = "C:\\Users\\tiffv\\Desktop\\CYBER\\Flipper_zero\\NFC_Python\\nfc_files\\Maison_5.nfc"
+date_file_5 = ""
+home_5_file_path = ""
